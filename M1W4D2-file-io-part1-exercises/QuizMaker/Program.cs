38b6e2f930388159efde7501b5345191a9749c63
Dan Stockham <ds321609@gmail.com>
2017-05-30 17:52:05 -0400

    WordSearch completed

--- M1W4D2-file-io-part1-exercises/QuizMaker/Program.cs
+++ M1W4D2-file-io-part1-exercises/QuizMaker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace QuizMaker
 {
@@ -10,6 +11,49 @@ namespace QuizMaker
     {
         static void Main(string[] args)
         {
+            string dir = Environment.CurrentDirectory;
+            string file = "sample-quiz-file.txt";
+            string filePath = Path.Combine(dir, file);
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                int marks = 0;
+                int totalQuestions = 0;
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split('?');
+                    totalQuestions++;
+                    string question = line[0];
+                    int answer = 0;
+                    List<string> choices = new List<string>();
+                    choices.AddRange(line[1].Split('|'));
+
+                    Console.WriteLine(question);
+                    foreach (string item in choices)
+                    {
+                        if (item.Contains('*'))
+                        {
+                            item.Replace("*", string.Empty);
+                            answer = choices.IndexOf(item);
+                        }
+                        Console.WriteLine(item);
+                    }
+                    string userInput = Console.ReadLine();
+
+                    if(int.Parse(userInput) == answer)
+                    {
+                        Console.WriteLine("RIGHT!!");
+                        marks++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that was not the right answer");
+                    }
+                }
+                Console.WriteLine($"You got {marks} answer(s) correct out of the total {totalQuestions} questions asked ");
+            }
+
+            
         }
     }
 }
--- M1W4D2-file-io-part1-exercises/WordSearch/Program.cs
+++ M1W4D2-file-io-part1-exercises/WordSearch/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WordSearch
 {
@@ -10,9 +11,66 @@ namespace WordSearch
     {
         static void Main(string[] args)
         {
-			//1. Ask the user for the search string
-			//2. Ask the user for the file path
-			//3. Open the file
+            List<string> linesFound = new List<String>();
+
+            //1. Ask the user for the search string
+            Console.WriteLine("Which word are you searching for in the document?");
+            string searchWord = Console.ReadLine();
+            Console.WriteLine("Is the word case sensitive? (Y)es or (N)o?");
+            bool caseCheck = "y" == Console.ReadLine().ToLower();
+
+            if(!caseCheck)
+            {
+                searchWord = searchWord.ToLower();
+            }
+
+            //2. Ask the user for the file path
+           
+
+            //3. Open the file
+            {
+                try {
+                    Console.WriteLine("What is the filepath of the document you want to search?");
+                    string dir = Environment.CurrentDirectory;
+                    string file = Console.ReadLine();
+                    string filepath = Path.Combine(dir, file);
+
+                    int lineNum = 1;
+                    using (StreamReader sr = new StreamReader(filepath))
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+
+                            if (!caseCheck)
+                            {
+                                line = line.ToLower();
+                            }
+
+
+                            if (line.Contains(searchWord))
+                            {
+                                linesFound.Add(lineNum + ") " + line);
+                        }
+
+                        lineNum++;
+                    }
+                }
+                catch(IOException e)
+                {
+                    Console.WriteLine("The file you entered either doesn't exist or is not within this directory");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+                
+            }
+
+            foreach(string line in linesFound)
+            {
+                Console.WriteLine(line);
+            }
 				//4. Loop through each line in the file
 					//5. If the line contains the search string, print it out along with its line number
 				
