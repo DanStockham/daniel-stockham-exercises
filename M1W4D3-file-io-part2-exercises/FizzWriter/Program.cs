686838e64ae35aaa3c9e70ee4335623683291a27
Dan Stockham <ds321609@gmail.com>
2017-05-31 21:31:10 -0400

    completed FizzWriter

--- M1W4D3-file-io-part2-exercises/FizzWriter/Program.cs
+++ M1W4D3-file-io-part2-exercises/FizzWriter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -10,6 +11,40 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
+            string dir = Environment.CurrentDirectory;
+            string outputFile = "fizzbuzz.txt";
+            string filePath = Path.Combine(dir, outputFile);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath, false))
+                {
+                    for(int i = 1; i <= 300; i++)
+                    {
+                        if(i % 3 == 0 && i % 5 == 0)
+                        {
+                            sw.WriteLine("FizzBuzz");
+                        }
+                        else if(i % 3 == 0)
+                        {
+                            sw.WriteLine("Fizz");
+                        }
+                        else if(i % 5 == 0)
+                        {
+                            sw.WriteLine("Buzz");
+                        }
+                        else
+                        {
+                            sw.WriteLine(i);
+                        }
+                    }
+
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Make sure the file is being created in a directory that exist");
+            }
         }
     }
 }
