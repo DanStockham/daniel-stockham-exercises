4589532c0c2524e1317993baef468f6626e74cbc
Dan Stockham <ds321609@gmail.com>
2017-06-27 22:03:48 -0400

    Baby Lotto compeleted

---
+++ M3W2D1-controllers-part1-exercises/Exercises.Web/Controllers/BabyLottoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercises.Web.Models;
+
+namespace Exercises.Web.Controllers
+{
+    public class BabyLottoController : Controller
+    {
+        // GET: BabyLotto
+        public ActionResult BabyLotto()
+        {
+            return View("BabyLotto");
+        }
+
+        public ActionResult BabyLottoResult(BabyLottoModel obj)
+        {
+            BabyLottoModel model = new BabyLottoModel();
+
+            model.LottoNum = obj.LottoNum;
+
+            return View("BabyLottoResults", model);
+        }
+    }
+}
---
+++ M3W2D1-controllers-part1-exercises/Exercises.Web/Models/BabyLottoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercises.Web.Models
+{
+    public class BabyLottoModel
+    {
+        public List<string> LottoNum { get; set; }
+
+    }
+}
