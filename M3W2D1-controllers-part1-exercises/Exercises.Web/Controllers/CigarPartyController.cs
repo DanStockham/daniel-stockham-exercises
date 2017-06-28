425a7e139dcae5c10eefc27a5d43e30930066685
Dan Stockham <ds321609@gmail.com>
2017-06-28 08:09:58 -0400

    day 1 complete

---
+++ M3W2D1-controllers-part1-exercises/Exercises.Web/Controllers/CigarPartyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercises.Web.Models;
+
+namespace Exercises.Web.Controllers
+{
+    public class CigarPartyController : Controller
+    {
+        // GET: CigarParty
+        public ActionResult CigarParty()
+        {
+            return View("CigarParty");
+        }
+        public ActionResult CigarPartyResult(CigarPartyModel obj)
+        {
+            CigarPartyModel model = new CigarPartyModel();
+            model.NumOfSquirrels = obj.NumOfSquirrels;
+            model.IsWeekend = obj.IsWeekend;
+
+            return View("CigarPartyResult", model);
+        }
+    }
+}
---
+++ M3W2D1-controllers-part1-exercises/Exercises.Web/Models/CigarPartyModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Exercises.Web.Models;
+
+namespace Exercises.Web.Models
+{
+    public class CigarPartyModel
+    {
+        public int NumOfSquirrels { get; set; }
+        public bool IsWeekend { get; set; }
+
+        public bool IsSuccess()
+        {
+            if(NumOfSquirrels >= 40 && NumOfSquirrels <= 60)
+            {
+                return true;
+            }
+            else if (NumOfSquirrels >= 40 && IsWeekend)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
