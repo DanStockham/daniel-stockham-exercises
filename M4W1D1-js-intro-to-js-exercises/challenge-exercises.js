7e7d7b3c180c1d005c9b7f92225db9b4367f6212
Dan Stockham <ds321609@gmail.com>
2017-07-11 16:17:22 -0400

    one last challenge

--- M4W1D1-js-intro-to-js-exercises/challenge-exercises.js
+++ M4W1D1-js-intro-to-js-exercises/challenge-exercises.js
@@ -2,9 +2,15 @@
     var strArr = str.split(' ');
     var even = 0;
     var odd = 0;
+    var numIdx;
+
+    if (strArr.length <= 1) {
+        numIdx = 0;
+        return numIdx;
+    }
 
     strArr.forEach(function (curr) {
-        if (curr % 2 === 0) {
+        if (parseInt(curr) % 2 === 0) {
             even++;
         } else {
             odd++;
@@ -14,16 +20,42 @@
     if (even === 1) {
         strArr.forEach(function (curr, idx) {
             if (curr % 2 === 0) {
-                return idx + 1
+                numIdx = idx + 1;
             }
         });
-    }
-
-    if (odd === 1) {
+    } else if(odd === 1) {
         strArr.forEach(function (curr, idx) {
             if (curr % 2 !== 0) {
-                return idx + 1;
+                numIdx = idx + 1;
             }
         });
+    } else {
+        numIdx = 0;
+        return numIdx;
     }
+
+    return numIdx;
+}
+
+function titleCase(str, minor) {
+    var lowerStr = str.toLowerCase().split(' ');
+    var minorArr
+
+    if (minor) {
+        minorArr = minor.toLowerCase().split(' ');
+        minorArr.join(' ');
+    }
+
+    var titleCase = lowerStr.map(function (curr) {
+        if (!minor || minorArr.indexOf(curr) === -1) {
+            curr = curr[0].toUpperCase() + curr.substring(1);
+            return curr
+        } else {
+            return curr;
+        }
+    })
+
+    titleCase[0] = titleCase[0][0].toUpperCase() + titleCase[0].slice(1);
+
+    return titleCase.join(' ');
 }
