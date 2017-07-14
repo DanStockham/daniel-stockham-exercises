15bb89500ce5ca5ffef1e0d3ccd3e6909f953e06
Dan Stockham <ds321609@gmail.com>
2017-07-13 21:06:25 -0400

    part 1 of validation complete

--- M4W1D2-csharp-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/validation-exercises.js
+++ M4W1D2-csharp-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/validation-exercises.js
@@ -2,7 +2,19 @@
 /// <reference path="../jquery.validate.js" />
 
 $(document).ready(function () {
+    $('#SameShipping').change(function() {
+        $('#ShippingAddress1').val($('#BillingAddress1').val());
+        $('#ShippingAddress2').val($('#BillingAddress2').val());
+        $('#ShippingCity').val($('#BillingCity').val());
+        $('#ShippingState').val($('#BillingState').val());
+        $('#ShippingPostalCode').val($('#BillingPostalCode').val());
 
+    });
+
+    $('input[name="ShippingType"]').change(function () {
+        console.log();
+        $('#shipping span').text($('input[name="ShippingType"]').data().cost);
+    })
    
 });
 
