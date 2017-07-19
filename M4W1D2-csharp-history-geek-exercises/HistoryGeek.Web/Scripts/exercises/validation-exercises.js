d47353ef06be755798d4d2ec3262d294de3964f3
Dan Stockham <ds321609@gmail.com>
2017-07-19 13:03:12 -0400

    checkout validation working

--- M4W1D2-csharp-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/validation-exercises.js
+++ M4W1D2-csharp-history-geek-exercises/HistoryGeek.Web/Scripts/exercises/validation-exercises.js
@@ -27,13 +27,65 @@ $(document).ready(function () {
             },
             BillingCity: {
                 required: true,
-
+                lettersonly: true
+            },
+            BillingState: {
+                required: true,
+                letterssonly: true
+            },
+            BillingPostalCode: {
+                required: true,
+                numbersonly: true,
+                minLength: 5
+            },
+            ShippingAddress1: {
+                required: true
+            },
+            ShippingCity: {
+                required: true,
+                lettersonly: true
+            },
+            ShippingState: {
+                required: true,
+                letterssonly: true
+            },
+            ShippingPostalCode: {
+                required: true,
+                numbersonly: true,
+                minLength: 5
+            },
+            NameOnCard: {
+                required: true,
+                lettersonly: true
+            },
+            CreditCardNumber: {
+                required: true,
+                numbersonly: true,
+                minLength: 16
+            },
+            ExpirationDate: {
+                required: true,
+                date: true
+            },
+            ShippingType: {
+                required: true
             }
-
         },
         messages: {
-            billingAddress1: "The address entered is not valid",
-            billingCity: "Please enter a valid city name"
+            BillingAddress1: "Please enter a valid billing address",
+            BillingCity: "Please enter a valid billing city name",
+            BillingState: "Please enter a valid state",
+            BillingPostalCode: "Please enter a valid zipcode",
+            ShippingAddress1: "Please enter a valid shipping address",
+            ShippingCity: "Please enter a valid shipping city",
+            ShippingState: "Please enter a valid shipping state",
+            ShippingPostalCode: "Please enter a valid shipping zipcode",
+            NameOnCard: "Please enter name how it is displayed on your card",
+            CreditCardNumber: "Please enter valid credit card number",
+            ExpirationDate: "Please enter a valid expiration date",
+            ShippingType: "Please enter a method of shipping"
+
+
         },
         errorClass: 'field-validation-error',
         errorLabelContainer: "#errors ",
